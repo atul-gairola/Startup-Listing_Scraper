da52f5346d9dbbe9b7823f8059cc6c052ea55868
Atul Gairola <atulgairola08@gmail.com>
2020-07-09 13:58:46 +0530

    set

--- index.js
+++ index.js
@@ -22,20 +22,20 @@ const vbProfileScraper = require('./vbProfiles');
     .catch((err) => console.log(err));
 
     // scrapping betalist
-    // await betaListScraper();
+    await betaListScraper();
 
     // scraping craft
     // await craftScraper();
 
     // scraping e27 
-    // await e27Scraper();
+    await e27Scraper();
 
     // scraping startupIndia
-    // await startupIndiaScraper();
+    await startupIndiaScraper();
 
 
     // scraping tracxn
-    // await tracxnScraper();
+    await tracxnScraper();
 
     // scraping vbProfiles
     await vbProfileScraper();
